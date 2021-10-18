7cf31251e5b60cd7c6f4818a661b34b946d000ce
chaiyaphat supharak <chaiyaphat.b5922314@gmail.com>
2021-10-18 21:40:34 +0700

    topic: varibles

--- variables/learn.go
+++ variables/learn.go
@@ -1,9 +1,33 @@
 package variables
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var fullname string //global
+var email string = "codingthailand@gmail.com"
+var c, python bool = true, false
+
+const pi = 3
 
 func Learn() {
-	fmt.Println("Learn var")
+	fmt.Println("-----------------func Learn()-----------------")
+	fmt.Printf("Hello %s EMail %s \n", fullname, email)
+	fmt.Println(c, python)
+
+	companyName := "ArtThailand"
+	isShow := true
+	fmt.Println(companyName)
+	fmt.Println(isShow)
+	fmt.Printf("type of isShow is %T \n", isShow)
+
+	age := 10
+	f := float64(age)
+	fmt.Printf("Type of f is %T \n", f)
+
+	s := strconv.Itoa(pi) //convert number to string
+	fmt.Println(s)
+
+	fmt.Println("---------------------END----------------------")
 }
