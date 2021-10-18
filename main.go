d6f34f3373a2f221c1646e96ce12630240011422
chaiyaphat supharak <chaiyaphat.b5922314@gmail.com>
2021-10-18 21:25:02 +0700

    varibles

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/chaiyaphat20/variables"
+)
+
+func main() {
+	variables.Learn()
+}
---
+++ variables/learn.go
@@ -0,0 +1,9 @@
+package variables
+
+import "fmt"
+
+var fullname string //global
+
+func Learn() {
+	fmt.Println("Learn var")
+}
